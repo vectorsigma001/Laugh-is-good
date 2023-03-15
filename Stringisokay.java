a05f2a086dae18937e6fd3bf8aec51f1cb40ab72
whereyoucan'tfind <74447376+vectorsigma001@users.noreply.github.com>
2023-03-15 22:52:41 +0545

    Create Stringisokay.java

---
+++ Stringisokay.java
@@ -0,0 +1,27 @@
+public class Main{
+  String str="knock";
+    
+    void plaindrome(){
+       int size=str.length();
+      if(size==0||size==1){ 
+        System.out.println("Hence it is palindrome");
+      }
+    else{ 
+        char c1=str.charAt(size-size);
+        char c2=str.charAt(size-1);
+        str=str.substring(size-size,size-1);
+        boolean c4=c1==c2;
+        if(c4==true){   
+          plaindrome();
+        }  
+       else{
+          System.out.println(false);
+       }
+    }
+          }     
+  public static void main(String args[]){
+   Main obj=new Main();
+     obj.plaindrome();
+    
+  }
+}
